2fd448b7e558519d7c55c1be21d61ca6eaa14c6d
René Sitchi <rene.sitchi@inetconsulting.cm>
2023-08-06 18:57:03 +0100

    add a picture for each dish o

--- src/services/cssLoaded.tsx
+++ src/services/cssLoaded.tsx
@@ -4,37 +4,13 @@ import { useEffect, useState } from 'react';
 const useAllCssLoaded = () => {
   const [cssFilesLoaded, setCssFilesLoaded] = useState<String[]>([]);
   const [allCssLoaded, setAllCssLoaded] = useState(false);
-
-  useEffect(() => {
-    const links = document.querySelectorAll('link[rel="stylesheet"]');
-    const totalCssFiles = links.length;
-    console.log(links);
-
-    const handleCssLoad = (event:Event) => {
-      const { href } = event.target as HTMLLinkElement ;
-      setCssFilesLoaded((prev) => {
-        if (cssFilesLoaded.length+1 === totalCssFiles) {
-            setAllCssLoaded(true);
-          }
-        return [...prev, href]
-     });
-
-      
-    };
-    let a:  HTMLLinkElement[]=[]
-    links.forEach(element => {
-        a.push(element as HTMLLinkElement)
-    });
-    a.map(x=>x.addEventListener('load',()=>console.log('loaded')
-    ))
-    // return () => {
-    //   links.forEach((link) => {
-    //     if (link.removeEventListener) {
-    //       link.removeEventListener('load', handleCssLoad);
-    //     }
-    //   });
-    // };
-  }, [cssFilesLoaded]);
+  const links = document.querySelectorAll('link[rel="stylesheet"]');
+  let a:  HTMLLinkElement[]=[]
+  links.forEach(element => {
+      a.push(element as HTMLLinkElement)
+  });
+  a.map(x=>x.addEventListener('load',()=>console.log('loaded')
+  ))
 
   return allCssLoaded;
 };
